ee31320bdf5bc687b5f8adb8e4a6392123f30c41
MilesDev91 <34494229+MilesDev91@users.noreply.github.com>
2021-11-22 02:52:05 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app = express();
 app.use(serveStatic(path.join(__dirname, 'dist')));
 const port = process.env.PORT || 80;
 app.get("*", (req, res) => {
-  let url = path.join(__dirname, '../client/build', 'index.html');
+  let url = path.join(__dirname, 'index.html');
   if (!url.startsWith('/app/')) // since we're on local windows
     url = url.substring(1);
   res.sendFile(url, { 'root': '../client/build' });
