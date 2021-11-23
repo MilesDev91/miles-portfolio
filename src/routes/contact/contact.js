59ef3e4ef30cf34b43285943bc9faecc16b1c56d
MilesDev91 <34494229+MilesDev91@users.noreply.github.com>
2021-11-23 01:46:43 -0500

    Fixing stuff for firebase and adjusting hours for the other viewport size

--- src/routes/contact/contact.js
+++ src/routes/contact/contact.js
@@ -13,7 +13,6 @@ export default class Contact extends Component {
                 <div className='info'>
                     <p><strong>Email:</strong> <br /><br />micoffin91@gmail.com</p>
                 </div>
-                <h4>I will try to respond outside of available hours if at all possible.</h4>
             </div>
         );
 
@@ -24,10 +23,7 @@ export default class Contact extends Component {
                         <h2>If you would like to get in touch:</h2>
                         <div className='info'>
                             <p><strong>Email:</strong> micoffin91@gmail.com</p>
-                            <p><strong>Hours:</strong> Mon-Fri 9:00am-12:30pm EST</p>
-                            <p className='hours'>Sat-Sun 11:00am-8:00pm EST</p>
                         </div>
-                        <h4>I will try to respond outside of available hours if at all possible.</h4>
                     </div>
 
                 );
