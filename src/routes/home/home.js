3ca2e45fbc70b7d2baa17d6959e32a9f33640f32
MilesDev91 <34494229+MilesDev91@users.noreply.github.com>
2022-01-27 22:24:13 -0500

    Updating home page
    Updated some content on the home page.
    
    Structured the html in a more sensible manner.
    
    Replaced useless carousel with a picture.

--- src/routes/home/home.js
+++ src/routes/home/home.js
@@ -1,71 +1,99 @@
-import { Component } from 'react';
-import './home.css';
+import { Component } from "react";
+import "./home.css";
+import aboutMeImg from "../../assets/image/aboutme.jpg";
 
-import Carousel from '../../components/Carousel/Carousel.js';
-import Header from '../../components/Header/Header';
-import Footer from '../../components/Footer/Footer';
-import withResizeHandler from '../../hoc/withResizeHandler';
+// import Carousel from "../../components/Carousel/Carousel.js";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import withResizeHandler from "../../hoc/withResizeHandler";
 
-const imgArray = ['https://s3.amazonaws.com/miles-portfolio.com/assets/image/Initial.jpeg', 'https://s3.amazonaws.com/miles-portfolio.com/assets/image/Second.png', 'https://s3.amazonaws.com/miles-portfolio.com/assets/image/Third.jpeg']
-const imgText = ['Every site has its unique challenges, and each can be overcome.', 'React.js transforms any website into a smooth experience.', 'Success through communication.']
+// const imgArray = [
+//   "https://s3.amazonaws.com/miles-portfolio.com/assets/image/Initial.jpeg",
+//   "https://s3.amazonaws.com/miles-portfolio.com/assets/image/Second.png",
+//   "https://s3.amazonaws.com/miles-portfolio.com/assets/image/Third.jpeg",
+// ];
+// const imgText = [
+//   "Every site has its unique challenges, and each can be overcome.",
+//   "React.js transforms any website into a smooth experience.",
+//   "Success through communication.",
+// ];
 
 class Home extends Component {
-
-	render () {
-		let images = imgArray.map((img, index) => (
-			<div className="crop" key={img}>
-				<div className="overlay" />
-				<img className="smallImage" src={img} alt="" />
-				<div className="imgText">
-					{imgText[index]}
-				</div>
-			</div>
-		));
-
-		if (typeof window !== 'undefined') {
-			if (window.matchMedia('(min-width: 600px)').matches) {
-				images = <div><Carousel images={imgArray} animationTiming={500} /></div>;
-			}
-		}
-
-		return (
-			<div>
-				<Header />
-				<div className="home">
-					{images}
-					<div className="information">
-						<div className="infoItem">
-							<h1>About Me</h1>
-							<p>Hello, I am a junior web developer who is quickly gaining practical experience with my own projects. I have always loved computers, and being a web developer makes that love even stronger. I am very resourceful when it comes to solving problems, and I don't give up easily. If I don't find a solution elsewhere, then I create one myself.</p>
-						</div>
-						<div className="infoItem">
-							<h1>Skills</h1>
-							<p>These are some of the various skills I have become familiar with so far. I have left out a bunch of things that I have come across but not used more than once or twice. Visit the <a href="/portfolio">portfolio page</a> to see how I used these skills in my projects. I am currently working on more original projects to better showcase my knowlege of these.</p>
-							<ul>
-								<li><strong>HTML, CSS, and Javascript</strong></li>
-								<p>The building blocks of the web. I am very familiar with these by now.</p>
-								<li><strong>Vue</strong></li>
-								<p>Vue.js is a powerful javascript framework that combines html and javascript into a well oiled machine. I love Vue for how sensible and powerful it is. This is what I make most of my projects in.</p>
-								<li><strong>React</strong></li>
-								<p>React is an awesome component based Node.js library/framework. It is what lead me to learning Vue, and it is a powerful framework in its own right. I would certainly not mind working with React if given the chance.</p>
-								<li><strong>PHP</strong></li>
-								<p>I am not greatly familiar with PHP, but I understand its concepts somewhat. I prefer client-side languages, but I would not be uncomfortable working with it, especially since it has good documentation.</p>
-								<li><strong>Git and Github</strong></li>
-								<p>Git is a version control system that is very useful for tracking and controlling changes to your app. I use it with every project I do and have become very familiar with how it works.</p>
-								<li><strong>NPM</strong></li>
-								<p>NPM (Node Package Manager) is a vast package manager for the Node.js javascript runtime.</p>
-								<li><strong>Databases</strong></li>
-								<p>I have knowledge of working with relational and non-relational databases. I am particularly familiar with Firebase at this point.</p>
-								<li><strong>Wordpress</strong></li>
-								<p>I have used wordpress in the past, and I would like to further explore wordpress with custom php modification.</p>
-							</ul>
-						</div>
-					</div>
-				</div>
-				<Footer />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="home">
+          <div className="information">
+            <div className="aboutMe">
+              <img className="aboutMeImage" src={aboutMeImg} />
+              <div>
+                <h1>About Me</h1>
+                <p>
+                  Hello, I am a web developer who is quickly gaining practical
+                  experience with my own projects. I have always loved
+                  computers, and being a web developer makes that love even
+                  stronger. I am very resourceful when it comes to solving
+                  problems, and I don't give up easily. If I don't find a
+                  solution elsewhere, then I create one myself.
+                </p>
+              </div>
+            </div>
+            <div className="infoItem">
+              <h1>Skills</h1>
+              <p>
+                These are some of the various skills I have become familiar with
+                so far. Visit the <a href="/portfolio">portfolio page</a> to see
+                how I used these skills in my projects. I am currently working
+                on more original projects to better showcase my knowlege of
+                these.
+              </p>
+              <ul>
+                <li>
+                  <h2 className="skillTitle">HTML, CSS, and Javascript</h2>
+                  <p>The building blocks of the web.</p>
+                </li>
+                <li>
+                  <h2 className="skillTitle">Vue</h2>
+                  <p>
+                    Vue.js is a great javascript framework that combines HTML
+                    and javascript into Vue's. Shares some of the same ideas as
+                    React, especially with component modularity.
+                  </p>
+                </li>
+                <li>
+                  <h2 className="skillTitle">React</h2>
+                  <p>
+                    React.js is a powerful component based Node.js library.
+                    React is designed with well-controlled state in mind, and it
+                    has great TypeScript support.
+                  </p>
+                </li>
+                <li>
+                  <h2 className="skillTitle">Git and Github</h2>
+                  <p>
+                    Git is a version control system that is very useful for
+                    tracking and controlling changes to your app.
+                  </p>
+                </li>
+                <li>
+                  <h2 className="skillTitle">MongoDB</h2>
+                  <p>
+                    MongoDB is one of the most popular non-relational databases
+                    out there, and for good reason. Mongoose allows for easy
+                    creation of models that structure the data for your
+                    application. It also has a plethora of built-in features and
+                    extensive documentation.
+                  </p>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default withResizeHandler(Home);
--- src/routes/portfolio/portfolio.js
+++ src/routes/portfolio/portfolio.js
@@ -1,94 +1,52 @@
-import { Component } from 'react';
-import './portfolio.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import PortfolioItem from '../../components/PortfolioItem/PortfolioItem';
-import Header from '../../components/Header/Header'
-import Footer from '../../components/Footer/Footer'
+import { Component } from "react";
+import "./portfolio.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import PortfolioItem from "../../components/PortfolioItem/PortfolioItem";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 
 export default class Portfolio extends Component {
-
-	render () {
-		return (
-			<div>
-				<Header />
-				<div className='portfolio'>
-					<h1><FontAwesomeIcon icon="folder-open" /> Portfolio <br /><hr /></h1>
-
-					{/* This section is for solo projects */}
-					<div className='portfolioItems'>
-						<PortfolioItem
-							imageWidth="400"
-							imageHeight="300"
-							image="https://s3.amazonaws.com/miles-portfolio.com/assets/image/JnsMetals.png"
-							link="https://jnsmetalart.com"
-							title="JnS Metal Art"
-							content="JnS Metals is a live production app that I made for family. When business needs pick up production of the app will continue (or transition to another platform if appropriate), in which full e-commerce functionality is intended. It is hosted by Inmotion and uses Amazon S3 and a Firebase realtime database for its data."
-							github="https://github.com/MilesDev91/snjmetals"
-						/>
-						<PortfolioItem
-							imageWidth="400"
-							imageHeight="300"
-							image="https://s3.amazonaws.com/miles-portfolio.com/assets/image/TalkGaming.png"
-							link="https://talk-gaming.herokuapp.com/#/"
-							title="Talk Gaming"
-							content="This is a gaming forum I am creating in Vue. It will eventually have a small set of features that most forums have. I made it particularly to practice database management with Firestore."
-							github="https://github.com/MilesDev91/talkgaming"
-						/>
-						<PortfolioItem
-							imageWidth="400"
-							imageHeight="300"
-							image="https://s3.amazonaws.com/miles-portfolio.com/assets/image/SudokuSolver.png"
-							link="https://talk-gaming.https://solve-sudoku-application.herokuapp.com/.com/#/"
-							title="Sudoku Solver"
-							content="This is a bit of a double hobby project that I thought would be good practice for algorithm and basic UI development. It is made in Vue as well. It currently has basic input and validity checking functionality, and I intend to give it a nice solving feature set that is unique to other sudoku apps."
-							github="https://github.com/MilesDev91/sudokusolver"
-						/>
-					</div>
-
-					{/* This section is for tutorial projects */}
-					<h2>Tutorial Projects</h2>
-					<div className='portfolioItems'>
-						<PortfolioItem
-							imageWidth="400"
-							imageHeight="300"
-							image="https://s3.amazonaws.com/miles-portfolio.com/assets/image/StoryBooks.png"
-							link="https://storybooks1.herokuapp.com/"
-							title="StoryBooks"
-							content="StoryBooks is an app for writing and sharing stories that I made following a Node.js/Express/MongoDB course by Brad Traversy. It uses Express with handlebars for its framework, MongoDB via mongoose as its database, and Google oAuth as its authentification. Building this app taught me a lot about handling google authentification with express."
-						/>
-
-						<PortfolioItem
-							imageWidth="400"
-							imageHeight="300"
-							image="https://s3.amazonaws.com/miles-portfolio.com/assets/image/SharePosts.png"
-							link="http://miles-applications.com/SharePosts/"
-							title="SharePosts"
-							content="SharePosts is a simple social board app that I made during a PHP(OOP & MVC) course by Brad Traversy. It uses a custom MVC framework, a mySQL database, and uses a session based authentification system. This app taught me about the php language, htaccess, session variables, and how to use a MVC framework."
-						/>
-					</div>
-					<div className='portfolioItems2'>
-						<PortfolioItem
-							imageWidth="400"
-							imageHeight="300"
-							image="https://s3.amazonaws.com/miles-portfolio.com/assets/image/Vidjot.png"
-							link="https://vidjot2.herokuapp.com/"
-							title="Vidjot"
-							content="Vidjot is an app for writing down quick video ideas. I made it following a Node.js/Express/MongoDB course by Brad Traversy. It uses Express with handlebars for its framework, MongoDB via mongoose as its database, and passport as its authentification. This app introduced me to the express framework and taught me about building authentication strategies."
-						/>
-
-						<PortfolioItem
-							imageWidth="400"
-							imageHeight="300"
-							image="https://s3.amazonaws.com/miles-portfolio.com/assets/image/BurgerBuilder.png"
-							link="https://myburger-31b48.firebaseapp.com/"
-							title="Burger Builder"
-							content="Burger Builder is a burger building app that lets you login, build a burger, and order it. I made it following a React 16 course by Maximillian Schwarzmüller. This application uses the react framework, a firebase database, and firebase authentification. It introduced me to the React framework (and very well, it was a great course) and a few of its tools, such as redux and axios. I learned everything from lifecyle hooks to redux state management, and I liked it so much I used react to build www.miles-applications.com."
-						/>
-					</div>
-
-				</div>
-				<Footer />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <Header />
+        <div className="portfolio">
+          <h1>
+            <FontAwesomeIcon icon="folder-open" /> Portfolio <br />
+            <hr />
+          </h1>
+          <div className="portfolioItems">
+            <PortfolioItem
+              imageWidth="400"
+              imageHeight="300"
+              image="https://s3.amazonaws.com/miles-portfolio.com/assets/image/JnsMetals.png"
+              link="https://jnsmetalart.com"
+              title="JnS Metal Art"
+              content="JnS Metals is a live production app that I made for family. When business needs pick up production of the app will continue (or transition to another platform if appropriate), in which full e-commerce functionality is intended. It is hosted by Inmotion and uses Amazon S3 and a Firebase realtime database for its data."
+              github="https://github.com/MilesDev91/snjmetals"
+            />
+            <PortfolioItem
+              imageWidth="400"
+              imageHeight="300"
+              image="https://s3.amazonaws.com/miles-portfolio.com/assets/image/TalkGaming.png"
+              link="https://talk-gaming.herokuapp.com/#/"
+              title="Talk Gaming"
+              content="This is a gaming forum I am creating in Vue. It will eventually have a small set of features that most forums have. I made it particularly to practice database management with Firestore."
+              github="https://github.com/MilesDev91/talkgaming"
+            />
+            <PortfolioItem
+              imageWidth="400"
+              imageHeight="300"
+              image="https://s3.amazonaws.com/miles-portfolio.com/assets/image/SudokuSolver.png"
+              link="https://talk-gaming.https://solve-sudoku-application.herokuapp.com/.com/#/"
+              title="Sudoku Solver"
+              content="This is a bit of a double hobby project that I thought would be good practice for algorithm and basic UI development. It is made in Vue as well. It currently has basic input and validity checking functionality, and I intend to give it a nice solving feature set that is unique to other sudoku apps."
+              github="https://github.com/MilesDev91/sudokusolver"
+            />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
 }
