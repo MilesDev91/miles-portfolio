17d3aa7be22d240461a7c49c959a5e2045e9f2ba
MilesDev91 <34494229+MilesDev91@users.noreply.github.com>
2022-02-26 11:49:21 -0500

    Update talkgaming.js

--- src/routes/portfolio/talkgaming/talkgaming.js
+++ src/routes/portfolio/talkgaming/talkgaming.js
@@ -23,6 +23,28 @@ export default class TalkGaming extends Component {
             is fine. This was meant to be an exercise more than a proof of
             concept or anything like that.
           </p>
+          <h2>Application structure/Data flow</h2>
+          <p>
+            I really wanted to structure my application to retrieve and send
+            data from views, using a vuex store as my single source of truth.
+            Any components that are used in the views themselves receive and
+            pass data from and to their parent view components. I did this to
+            ensure data was flowing in a predictable direction.
+          </p>
+          <h2>Posts</h2>
+          <p>
+            Posts are fairly straight forward. They are currently needed in two
+            variations, grouped and non-grouped.
+          </p>
+          <h2>Comments</h2>
+          <p>
+            Comments are a bit of a different beast, primarily because I want to
+            create trees of comments instead of all comments branching straight
+            from the post. My current plan is to use a v-for loop to recursively
+            loop through comments, passing down an incremental indent value to
+            visually represent children of comments. This is currently the
+            biggest challenge of this project.
+          </p>
         </div>
         <Footer />
       </div>
